7952e5685b0566c3bf133f8a9ffebfaba5fe87a6
tejasvijb <jbtejasvi@gmail.com>
2024-02-02 19:00:26 +0530

    findOneByFood

--- myApp.js
+++ myApp.js
@@ -40,8 +40,12 @@ const findPeopleByName = (personName, done) => {
 }
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
-};
+    Person.findOne({favoriteFoods: food}, (err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  })
+}
+
 
 const findPersonById = (personId, done) => {
   done(null /*, data*/);
