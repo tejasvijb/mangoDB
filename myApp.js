af4f7a6c33a6f85af01466363d8081d13f91b698
tejasvijb <jbtejasvi@gmail.com>
2024-02-03 23:10:42 +0530

    findEditThenSave

--- myApp.js
+++ myApp.js
@@ -57,8 +57,19 @@ const findPersonById = (personId, done) => {
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
+  Person.findById(personId, (err, person) => {
+    if (err) return console.error(err);
 
-  done(null /*, data*/);
+    person.favoriteFoods.push(foodToAdd)
+
+    person.save((err, updatedPerson) => {
+        if(err) return console.error(err);
+        done(null, updatedPerson)
+    })
+
+  })
+
+
 };
 
 const findAndUpdate = (personName, done) => {
